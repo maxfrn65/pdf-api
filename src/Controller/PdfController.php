f8f2e4c4ccbb66acca634eefeeb8c1bc305edefd
maxime <m.fourna@icloud.com>
2024-05-21 11:45:33 +0200

    fix: renamed controller file

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HtmlToPdfController extends AbstractController
+class PdfController extends AbstractController
 {
     #[Route('/pdf/api', name: 'app_pdf_api')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HtmlToPdfController.php',
+            'path' => 'src/Controller/PdfController.php',
         ]);
     }
 }

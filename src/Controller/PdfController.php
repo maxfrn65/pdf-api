9ec8eee20b8e0f36456dcf569f1c79b35a90c41b
maxime <m.fourna@icloud.com>
2024-05-21 12:27:46 +0200

    feat (wip): htmlToPdf function

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -3,17 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PdfController extends AbstractController
 {
-    #[Route('/pdf/api', name: 'app_pdf_api')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PdfController.php',
+    #[Route('/pdf', name: 'app_pdf')]
+    public function htmlToPdf(HttpClientInterface $client, ParameterBagInterface $param) {
+
+        $service_url=$param->get('service_url');
+
+        $response = $client->request(
+            'POST',
+            $url_service.'/forms/chromium/convert/html', [
+                'headers' => [
+                    'Content-Type' => 'multipart/form-data'
+                ],
+                'body' => $file
         ]);
     }
 }
